5db8e17723fdf6b4d235e407ababfc365a8317cc
= <monkeis7@gmail.com>
2021-11-13 20:57:56 +0900

    순위 & 일정 라우팅 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import { Tabs, Tab } from '@mui/material';
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { Tabs, Tab } from '@mui/material'
 
 function Header () {
+  const history = useHistory()
   const [selectedTab, setSelectedTab] = React.useState('standings')
   const handleChange = (event: React.SyntheticEvent, newSelectedTab: string) => {
     setSelectedTab(newSelectedTab)
+    history.push(newSelectedTab)
   }
 
   return (
-    <div>
+    <div className="header">
       <div className="title-area">
         <h1>해축해축</h1>
       </div>
       <Tabs value={selectedTab} onChange={handleChange} textColor="inherit" centered>
-        <Tab label="순위" value="standings"></Tab>
-        <Tab label="일정" value="fixtures"></Tab>
+        <Tab label="순위" value="standings" />
+        <Tab label="일정" value="fixtures" />
       </Tabs>
     </div>
   )
---
+++ src/pages/Standings.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Standings() {
+  return (
+    <div>
+      <h1>Standings!!</h1>
+    </div>
+  )
+}
+
+export default Standings
