8830dcf00043135250fb8fad1f238a1fcb5d2162
JongHoo Hwang <monkeis7@gmail.com>
2021-11-27 00:42:46 +0900

    API를 통한 data fetching (#2)
    * default path 로 Standings 설정
    
    * API를 통한 data fetching
    - axios 를 이용한 람다 호출 및 데이터 fetching
    - useEffect, useStage 적용
    - 순위 데이터 interface 정의

--- src/pages/Standings.tsx
+++ src/pages/Standings.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+
+interface Standing {
+  position: number,
+  teamName: string
+}
+
+interface ApiResponse {
+  data: Array<Standing>
+}
 
 function Standings() {
+  const [standingList, setStandingList] = useState<Array<Standing>>([])
+
+  const getStandingList = async () => {
+    try {
+      const result: ApiResponse = await Axios.get('https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/standings/LALIGA/2021')
+      setStandingList(result.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getStandingList()
+  }, [])
+
   return (
     <div>
       <h1>Standings!!</h1>
+      <table>
+        <tbody>
+        {
+          standingList.map((standing) => (
+            <tr key={standing.position}>
+              <td>{standing.position}</td>
+              <td>{standing.teamName}</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
     </div>
   )
 }
