68275341b0d88c375525fa9f813b07bbc1240818
= <monkeis7@gmail.com>
2021-11-29 01:16:26 +0900

    Dark Theme 적용 - MUI ThemeProvider 를 이용한 다크 모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Header from './components/Header'
 import Standings from './pages/Standings'
 import Fixtures from './pages/Fixtures'
 import './App.css'
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 function App () {
   return (
     <div className="App">
-      <Header />
-      <Route exact path="/">
-        <Redirect to="/Standings" />
-      </Route>
-      <Route path="/Standings" component={Standings} />
-      <Route path="/Fixtures" component={Fixtures} />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Route exact path="/">
+          <Redirect to="/Standings" />
+        </Route>
+        <Route path="/Standings" component={Standings} />
+        <Route path="/Fixtures" component={Fixtures} />
+      </ThemeProvider>
     </div>
   )
 }
