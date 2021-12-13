9255ca90bbd02f22264feee11c019cf1087d7a30
= <monkeis7@gmail.com>
2021-12-14 00:13:07 +0900

    일정 및 결과 인터페이스 추가

---
+++ src/interfaces/Fixture.ts
@@ -0,0 +1,10 @@
+export interface Fixture {
+  date: string,
+  round: number,
+  homeTeam: string,
+  homeTeamLogo: string,
+  homeTeamGoals: number,
+  awayTeam: string,
+  awayTeamLogo: string,
+  awayTeamGoals: number
+}
