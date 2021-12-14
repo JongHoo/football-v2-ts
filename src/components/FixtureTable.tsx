3630a3cdd04c6a539ef5105edaf9b8614ecc8c95
= <monkeis7@gmail.com>
2021-12-15 02:36:57 +0900

    팀별 일정 테이블 추가 - 일정 데이터 불러오기 - 스타일 적용

---
+++ src/components/FixtureTable.tsx
@@ -0,0 +1,56 @@
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material'
+import React from 'react'
+import { Fixture } from '../interfaces/Fixture'
+
+interface FixtureTableProps {
+  fixtureList: Array<Fixture>
+}
+
+function formatTime (dateString: string): string {
+  const tempDate = new Date(dateString)
+  const korDate = new Date(tempDate.setHours(tempDate.getHours() + 9))
+  return korDate.toISOString().replace('T', ' ').substr(5, 11)
+}
+
+function FixtureTable ({ fixtureList }: FixtureTableProps) {
+  return (
+    <TableContainer component={Paper} className='dark-table'>
+      <Table stickyHeader size='small'>
+        <TableHead>
+          <TableRow>
+            <TableCell align="right" style={{ minWidth: 135 }}>홈팀</TableCell>
+            <TableCell align="center" style={{ minWidth: 70 }}>vs</TableCell>
+            <TableCell align="left" style={{ minWidth: 135 }}>원정팀</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            fixtureList.map((row) => (
+              <TableRow key={row.round}>
+                <TableCell align="right">
+                  <div className='team-name' style={{ justifyContent: 'flex-end' }}>
+                    <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', width: 122 }}>{row.homeTeam}</div>
+                    <img src={row.homeTeamLogo} alt='team logo' style={{ marginLeft: 10, marginRight: 0 }} />
+                  </div>
+                </TableCell>
+                <TableCell align="center">
+                  <div className='round-info'>{`${row.round} R`}</div>
+                  <div>{`${row.homeTeamGoals ?? '-'} : ${row.awayTeamGoals ?? '-'}`}</div>
+                  <div className='round-info'>{formatTime(row.date)}</div>
+                </TableCell>
+                <TableCell align="left">
+                  <div className='team-name'>
+                    <img src={row.awayTeamLogo} alt='team logo' />
+                    <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', width: 122 }}>{row.awayTeam}</div>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default FixtureTable
--- src/pages/Fixtures.tsx
+++ src/pages/Fixtures.tsx
@@ -5,6 +5,8 @@ import Axios from "axios";
 import LeagueSelect from "../components/LeagueSelect";
 import SeasonSelect from "../components/SeasonSelect";
 import TeamSelect from "../components/TeamSelect";
+import FixtureTable from "../components/FixtureTable";
+import {Fixture} from "../interfaces/Fixture";
 
 function getCurrentSeason (): number {
   const today = new Date()
@@ -13,6 +15,7 @@ function getCurrentSeason (): number {
 
 function Fixtures () {
   const [teamList, setTeamList] = useState<Standing[]>([])
+  const [fixtureList, setFixtureList] = useState<Fixture[]>([])
   const [league, setLeague] = useState<string>('PL')
   const [season, setSeason] = useState<string>(getCurrentSeason().toString())
   const [team, setTeam] = useState<string>('')
@@ -24,6 +27,16 @@ function Fixtures () {
       const sortedTeamList = result.data.sort((a, b) => a.teamName.localeCompare(b.teamName))
       setTeamList(sortedTeamList)
       setTeam(sortedTeamList[0].teamName)
+    } catch (err) {
+      alert('데이터 가져오기 실패')
+      console.log(err)
+    }
+  }
+
+  const getFixtureList = async () => {
+    try {
+      const result: ApiResponse<Fixture> = await Axios.get(`https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/fixtures/${league}/${season}/${team}`)
+      setFixtureList(result.data)
     } catch (err) {
       alert('데이터 가져오기 실패')
       console.log(err)
@@ -46,12 +59,21 @@ function Fixtures () {
     getTeamList()
   }, [league, season])
 
+  useEffect(() => {
+    if (team) {
+      getFixtureList()
+    }
+  }, [team])
+
   return (
     <div className='content-area'>
       <div className="search-area">
         <LeagueSelect selectedLeague={league} onChange={changeLeague} />
         <SeasonSelect currentSeason={currentSeason} selectedSeason={season} onChangeSeason={changeSeason} />
         <TeamSelect selectedTeam={team} teamList={teamList} onChange={changeTeam} />
+      </div>
+      <div className='table-area'>
+        <FixtureTable fixtureList={fixtureList} />
       </div>
     </div>
   )
