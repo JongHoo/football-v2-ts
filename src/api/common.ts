e871d73103a07ff85404accfec97be046991d54d
= <monkeis7@gmail.com>
2022-02-20 17:35:01 +0900

    Axios Instance 에 BaseUrl 설정

--- src/api/common.ts
+++ src/api/common.ts
@@ -4,18 +4,22 @@ import { Standing } from '../interfaces/Standing'
 import { TopScorer } from '../interfaces/TopScorer'
 import { Fixture } from '../interfaces/Fixture'
 
+const apiInstance = Axios.create({
+  baseURL: 'https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com'
+})
+
 const commonApiList = {
   async getStandingList (league: string, season: string): Promise<ApiResponse<Standing>> {
-    return Axios.get(`https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/standings/${league}/${season}`)
+    return apiInstance.get(`/dev/standings/${league}/${season}`)
   },
   async getTopScorerList (league: string, season: string): Promise<ApiResponse<TopScorer>> {
-    return Axios.get(`https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/topscorers/${league}/${season}`)
+    return apiInstance.get(`/dev/topscorers/${league}/${season}`)
   },
   async getTeamList (league: string, season: string): Promise<ApiResponse<Standing>> {
-    return Axios.get(`https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/standings/${league}/${season}`)
+    return apiInstance.get(`/dev/standings/${league}/${season}`)
   },
   async getFixtureList (league: string, season: string, team: string): Promise<ApiResponse<Fixture>> {
-    return Axios.get(`https://54s8quvzrl.execute-api.ap-northeast-2.amazonaws.com/dev/fixtures/${league}/${season}/${team}`)
+    return apiInstance.get(`/dev/fixtures/${league}/${season}/${team}`)
   }
 }
 
